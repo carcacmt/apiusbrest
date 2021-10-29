355efa971555bcd75458a819c592adef60248e6b
Carlos <caceresmt481@gmail.com>
2021-10-29 08:34:32 -0500

    api con peticiones

--- index.js
+++ index.js
@@ -20,18 +20,64 @@
 
 })
 
+app.get('/api/producto/:id',async(req,res)=>{
 
- app.post("/api/persona",(req,res)=>{
+    const sql="select * from producto where id=?"
+    const id=req.params.id
+    db.query(sql,id,(err,data)=>{
+        if(err) return err
+        res.json({
+            "productos":data,
+            "status":"OK"
+        })
+    })
+
+})
+
+
+ app.post("/api/producto/save",(req,res)=>{
+     values=Object.values(req.body)
+     const sql = "insert into producto(nombre,color,precio) values(?,?,?)"
+     
+     db.query(sql,values,(err,data)=>{
+          if(err) return err
+          res.json({
+              "mensaje":"Producto Grabado",
+              "peticion":"OK"
+          })          
+      })
+      res.status(201)
 
  })
 
 
- app.put("/api/persona",(req,res)=>{
+ app.put("/api/producto/save",(req,res)=>{
+
+    values=Object.values(req.body)
+    const sql = "update producto set nombre=?,color=?,precio=? where id=?"
+    val=[values[1],values[2],values[3],values[0]]
+    db.query(sql,val,(err,data)=>{
+        if(err) return err
+        res.json({
+            "mensaje":"Producto Actualizado",
+            "peticion":"OK"
+        })          
+    })
+    res.status(201)
 
 })
 
-app.delete("/api/persona/:id",(req,res)=>{
+app.delete("/api/producto/:id",(req,res)=>{
 
+    const id=req.params.id
+    const sql ="delete from producto where id=?"
+    db.query(sql,id,(err,data)=>{
+        if(err) return err   
+        res.json({
+            "peticion":"OK"
+        })
+    })
+    res.status(204)
 })
  
 
